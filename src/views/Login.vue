20d2bf705e4e9dd56dbffeb6129b1a0e36a2c861
David Alen <davidalen51@gmail.com>
2021-04-16 00:17:54 -0300

    (add) login skeleton page

---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <div>Login page</div>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Login",
+});
+</script>
