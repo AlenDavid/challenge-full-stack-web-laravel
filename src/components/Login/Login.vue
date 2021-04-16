b3657f4e01b01f9c3be2e694009f4e8ca2476404
David Alen <davidalen51@gmail.com>
2021-04-16 15:55:22 -0300

    (add) login form component

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container>
+    <v-form @submit.prevent="onsubmit">
+      <v-text-field
+        label="E-mail"
+        type="email"
+        v-model="email"
+        :rules="emailRules"
+        required
+      />
+      <v-text-field label="Password" type="password" v-model="password" />
+      <v-row>
+        <v-col>
+          <v-btn>Cancel</v-btn>
+        </v-col>
+        <v-col>
+          <v-btn type="submit" :loading="loading" color="primary">Login</v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Login",
+  props: {
+    email: String,
+    password: String,
+  },
+  data: () => ({
+    emailRules: [
+      (email: string) => !!email || "E-mail is required.",
+      (email: string) =>
+        /^([\w\d]+)@([\w\d]+.)+[\w\d.]+([\w\d])+$/.test(email) ||
+        "E-mail is invalid.",
+    ],
+    loading: false,
+  }),
+  methods: {
+    onsubmit() {
+      this.loading = true;
+    },
+  },
+});
+</script>
