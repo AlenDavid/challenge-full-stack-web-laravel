80990ab25ef1f319a7c9a565c702ef08caf5b43d
David Alen <davidalen51@gmail.com>
2021-04-15 22:06:14 -0300

    (add) helloworld to storybook

---
+++ src/components/HelloWorld.stories.ts
@@ -0,0 +1,17 @@
+import HelloWorld from "./HelloWorld.vue";
+
+export default {
+  title: "Components/HelloWorld",
+  component: HelloWorld,
+};
+
+const Template = (args: any, { argTypes }: any) => ({
+  props: Object.keys(argTypes),
+  components: { HelloWorld },
+  template: "<hello-world v-bind='$props' />",
+});
+
+export const Default: any = Template.bind({});
+Default.args = {
+  title: "Título",
+};
