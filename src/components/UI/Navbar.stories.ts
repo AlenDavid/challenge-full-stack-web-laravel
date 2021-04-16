b761b98012722fd27524003d45dadeaa5d975f77
David Alen <davidalen51@gmail.com>
2021-04-16 13:21:34 -0300

    (add) navbar story

---
+++ src/components/UI/Navbar.stories.ts
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar.vue";
+
+export default {
+  title: "Components/Navbar",
+  component: Navbar,
+};
+
+const Template = (args: any, { argTypes }: any) => ({
+  props: Object.keys(argTypes),
+  components: { Navbar },
+  template: "<navbar v-bind='$props' />",
+});
+
+export const Default: any = Template.bind({});
