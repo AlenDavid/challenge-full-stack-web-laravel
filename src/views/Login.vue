015a71b0bdbc142aaabab83b062c05fe1f5e6e44
David Alen <davidalen51@gmail.com>
2021-04-17 18:11:40 -0300

    (update) login page
    using login component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,38 @@
 <template>
-  <v-container>
-    <div>Login page</div>
+  <v-container class="container" fluid>
+    <v-row no-gutters align="center" class="row-center">
+      <v-spacer />
+      <v-col>
+        <h1 class="center">Login</h1>
+        <login-component />
+      </v-col>
+      <v-spacer />
+    </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import LoginComponent from "@/components/Login/Login.vue";
 
 export default Vue.extend({
   name: "Login",
+  components: {
+    LoginComponent,
+  },
 });
 </script>
+
+<style lang="scss" scoped>
+.center {
+  text-align: center;
+}
+
+.container {
+  height: 100%;
+}
+
+.row-center {
+  height: 80%;
+}
+</style>
