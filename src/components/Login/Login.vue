de05699f992068690a46385dac94f167329280d5
David Alen <davidalen51@gmail.com>
2021-04-17 18:12:24 -0300

    (fix) login form ref
    this trigger a validation on form

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,22 +1,23 @@
 <template>
   <v-container>
-    <v-form @submit.prevent="onsubmit">
+    <v-form ref="ref" lazy-validation v-model="valid">
       <v-text-field
         label="E-mail"
         type="email"
         v-model="email"
         :rules="emailRules"
         required
-      />
-      <v-text-field label="Password" type="password" v-model="password" />
-      <v-row>
-        <v-col>
-          <v-btn>Cancel</v-btn>
-        </v-col>
-        <v-col>
-          <v-btn type="submit" :loading="loading" color="primary">Login</v-btn>
-        </v-col>
-      </v-row>
+      ></v-text-field>
+      <v-text-field
+        label="Password"
+        type="password"
+        v-model="password"
+        :rules="passwordRules"
+        required
+      ></v-text-field>
+      <v-btn x-large block :disabled="!valid" @click="validate" color="primary"
+        >Login</v-btn
+      >
     </v-form>
   </v-container>
 </template>
@@ -26,22 +27,25 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "Login",
-  props: {
-    email: String,
-    password: String,
-  },
   data: () => ({
+    ref: {},
+    email: "",
+    password: "",
+    valid: true,
     emailRules: [
       (email: string) => !!email || "E-mail is required.",
       (email: string) =>
         /^([\w\d]+)@([\w\d]+.)+[\w\d.]+([\w\d])+$/.test(email) ||
         "E-mail is invalid.",
     ],
+    passwordRules: [
+      (password: string) => !!password || "Password is required.",
+    ],
     loading: false,
   }),
   methods: {
-    onsubmit() {
-      this.loading = true;
+    validate() {
+      this.valid = (this.$refs.ref as any).validate();
     },
   },
 });
