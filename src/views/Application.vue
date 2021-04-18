dd3b86409d357a6dd2ed0a86fb5276bb06af0e94
David Alen <davidalen51@gmail.com>
2021-04-18 02:57:44 -0300

    (add) application view with navigation drawer

---
+++ src/views/Application.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<navigation />
+		<router-view />
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import Navigation from "@/components/UI/Navigation.vue"
+
+export default Vue.extend({
+	name: "App",
+	components: {
+		Navigation,
+	},
+})
+</script>
