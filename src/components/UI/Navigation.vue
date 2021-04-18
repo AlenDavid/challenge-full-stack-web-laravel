2ab03b90052c0b4761532260d7ff6f742e01e147
David Alen <davidalen51@gmail.com>
2021-04-18 02:57:25 -0300

    (add) navigation drawer skeleton
    this file is going to be updated with more fields to be enabled

---
+++ src/components/UI/Navigation.vue
@@ -0,0 +1,44 @@
+<template>
+	<v-navigation-drawer app permanent expand-on-hover>
+		<v-list>
+			<v-list-item link>
+				<v-list-item-content>
+					<v-list-item-title class="title"> Sandra Adams </v-list-item-title>
+					<v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+					<v-list-item-content>Manager</v-list-item-content>
+				</v-list-item-content>
+			</v-list-item>
+		</v-list>
+
+		<v-divider></v-divider>
+
+		<v-list nav dense>
+			<v-list-item link>
+				<v-list-item-icon>
+					<v-icon>mdi-folder</v-icon>
+				</v-list-item-icon>
+				<v-list-item-title>My Files</v-list-item-title>
+			</v-list-item>
+			<v-list-item link>
+				<v-list-item-icon>
+					<v-icon>mdi-account-multiple</v-icon>
+				</v-list-item-icon>
+				<v-list-item-title>Shared with me</v-list-item-title>
+			</v-list-item>
+			<v-list-item link>
+				<v-list-item-icon>
+					<v-icon>mdi-star</v-icon>
+				</v-list-item-icon>
+				<v-list-item-title>Starred</v-list-item-title>
+			</v-list-item>
+		</v-list>
+	</v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+	name: "Navigation",
+})
+</script>
