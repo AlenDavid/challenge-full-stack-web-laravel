2150e5c099bd45801e83b211889cc6aa785ff1bb
David Alen <davidalen51@gmail.com>
2021-04-17 21:18:53 -0300

    (add) students skeleton view

---
+++ src/views/Students.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>This is the students page</h1>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Students",
+});
+</script>
