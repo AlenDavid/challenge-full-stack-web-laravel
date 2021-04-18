b12169a0b72c551294cb7e336f12da7efd130124
David Alen <davidalen51@gmail.com>
2021-04-18 15:21:32 -0300

    (add) navigation drawer using user props

--- src/components/UI/Navigation.vue
+++ src/components/UI/Navigation.vue
@@ -1,11 +1,11 @@
 <template>
-	<v-navigation-drawer app permanent expand-on-hover>
+	<v-navigation-drawer expand-on-hover>
 		<v-list>
 			<v-list-item link>
 				<v-list-item-content>
-					<v-list-item-title class="title"> Sandra Adams </v-list-item-title>
-					<v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
-					<v-list-item-content>Manager</v-list-item-content>
+					<v-list-item-title class="title"> {{ user.name }} </v-list-item-title>
+					<v-list-item-subtitle> {{ user.email }}</v-list-item-subtitle>
+					<v-list-item-content>{{ type }}</v-list-item-content>
 				</v-list-item-content>
 			</v-list-item>
 		</v-list>
@@ -13,23 +13,23 @@
 		<v-divider></v-divider>
 
 		<v-list nav dense>
-			<v-list-item link>
+			<v-list-item link to="/app/students">
 				<v-list-item-icon>
-					<v-icon>mdi-folder</v-icon>
+					<v-icon>mdi-account-supervisor</v-icon>
 				</v-list-item-icon>
-				<v-list-item-title>My Files</v-list-item-title>
+				<v-list-item-title>Students</v-list-item-title>
 			</v-list-item>
-			<v-list-item link>
+			<v-list-item link to="/app/profile">
 				<v-list-item-icon>
-					<v-icon>mdi-account-multiple</v-icon>
+					<v-icon>mdi-face</v-icon>
 				</v-list-item-icon>
-				<v-list-item-title>Shared with me</v-list-item-title>
+				<v-list-item-title>Profile</v-list-item-title>
 			</v-list-item>
-			<v-list-item link>
+			<v-list-item link to="/app/settings">
 				<v-list-item-icon>
-					<v-icon>mdi-star</v-icon>
+					<v-icon>mdi-wrench</v-icon>
 				</v-list-item-icon>
-				<v-list-item-title>Starred</v-list-item-title>
+				<v-list-item-title>Settings</v-list-item-title>
 			</v-list-item>
 		</v-list>
 	</v-navigation-drawer>
@@ -37,8 +37,24 @@
 
 <script lang="ts">
 import Vue from "vue"
+import user from "@/store/modules/user"
 
 export default Vue.extend({
 	name: "Navigation",
+	computed: {
+		user: () => user.getters.user,
+		type: () => {
+			switch (user.getters.user.type) {
+				case 0:
+					return "Student"
+				case 1:
+					return "Manager"
+				case 2:
+					return "Admin"
+			}
+
+			return ""
+		},
+	},
 })
 </script>
