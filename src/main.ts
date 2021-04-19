1944e9b7a1387db4cb962401c26f7cac2d9ef754
David Alen <davidalen51@gmail.com>
2021-04-19 00:48:59 -0300

    (add) use VMask

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import App from "./App.vue"
 import vuetify from "./plugins/vuetify"
 import router from "./router"
 import AsyncComputed from "vue-async-computed"
+import VMask from "v-mask"
 
 Vue.config.productionTip = false
 
 Vue.use(AsyncComputed)
+Vue.use(VMask)
 
 new Vue({
 	vuetify,
