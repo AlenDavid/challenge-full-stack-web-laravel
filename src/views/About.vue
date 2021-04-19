9b28b46d90390d2ee4fa052f966256baf2f9d602
David Alen <davidalen51@gmail.com>
2021-04-19 05:32:39 -0300

    (add) about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,76 @@
 <template>
-	<div>
-		<h1>This is an about page</h1>
-	</div>
+	<v-container>
+		<v-row>
+			<v-col cols="6">
+				<span class="px-2 text-center text-h3">About the challenge</span>
+				<v-divider class="my-2" />
+				<div>
+					<p class="text-h5">
+						The students app challenge requires from the dev to show maestry in
+						some skills:
+					</p>
+					<p class="text-h4">Frontend,</p>
+					<p>
+						where it was asked - required, this is a challenge - to use
+						<a href="https://vuejs.org/">Vue</a>, a tool that quickly builds a
+						website.
+					</p>
+					<p class="text-h4">Backend,</p>
+					<p>
+						and now it was asked - required - to use
+						<a href="https://lumen.laravel.com/">Lumen</a>, and I love so much
+						this micro-framework ❤️!
+					</p>
+					<p class="text-h4">Database,</p>
+					<p>
+						we could use Postgres or MySQL, and I choose MySQL 'cause I can
+						query stuff quicker.
+					</p>
+					<p class="text-h4">Deployment,</p>
+					<p>
+						This was tricky and exceptional. If someone is fullstack it has to
+						know how to build stuff. But this person can create value for people
+						beyond it's own computer?
+					</p>
+					<p>last, but not least</p>
+					<p class="text-h4">Leadership.</p>
+					<p>
+						It's hard to find someone who can launch a ready-to-use app in less
+						then 30 hours of coding.
+					</p>
+					<p>
+						With the scope of one week (considering 2h/weekday, plus
+						8h/weekends, I only coulde get 26 hours of work), part of the
+						challenge was to determine what parts of the project scope must be
+						in production before other parts. This kind of decision is even
+						trickier then deployment 'cause requires knowledge in how much I can
+						code in such environment.
+					</p>
+				</div>
+			</v-col>
+			<v-col cols="6">
+				<span class="text-center text-h3">About the developer</span>
+				<v-divider class="my-2" />
+				<p class="text-h5">
+					The guy behind all this is David Alen - Me, of course. I'm 23 years
+					old, I live in Novo Hamburgo, state of Rio Grande do Sul, Brazil.
+				</p>
+				<p>
+					You can find me on my
+					<a href="https://twitter.com/davidalen_">Twitter</a>. Feel free to
+					reach me also on
+					<a href="https://linkedin.com/in/david-alen">LinkedIn</a> or
+					<a href="https://github.com/AlenDavid">Github</a>
+				</p>
+				<p>
+					I've been developing so much dope sites and mobile apps in the last
+					three years that makes me really proud of myself.
+				</p>
+				<p>
+					Still here? Go see the app! I put a lot of effort on learning Vue for
+					you to see this message!
+				</p>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
